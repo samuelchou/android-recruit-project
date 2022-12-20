8f4837eea460805375e9b6017b3b498f62312d7e
Samuel Chou <samuelchou@hotmail.com.tw>
2022-12-20 11:40:58 +0800

    feat(main/view): 示範 Data Binding.

--- app/src/main/java/in/hahow/android_recruit_project/MainActivity.kt
+++ app/src/main/java/in/hahow/android_recruit_project/MainActivity.kt
@@ -1,11 +1,22 @@
 package `in`.hahow.android_recruit_project
 
+import `in`.hahow.android_recruit_project.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        binding.textView.text = "Hello Data Binding!"
     }
 }
