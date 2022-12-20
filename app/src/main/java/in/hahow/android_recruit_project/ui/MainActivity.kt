2a505dcc0e3376b52c588634fa4b57ca05eb5165
Samuel Chou <samuelchou@hotmail.com.tw>
2022-12-20 16:51:29 +0800

    feat(list/view): 示範使用元件，並調整內容。

--- app/src/main/java/in/hahow/android_recruit_project/ui/MainActivity.kt
+++ app/src/main/java/in/hahow/android_recruit_project/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package `in`.hahow.android_recruit_project.ui
 
+import `in`.hahow.android_recruit_project.R
 import `in`.hahow.android_recruit_project.databinding.ActivityMainBinding
+import `in`.hahow.android_recruit_project.databinding.ItemCourseListBinding
 import `in`.hahow.android_recruit_project.viewModel.CourseListViewModel
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -42,6 +47,35 @@ class MainActivity : AppCompatActivity() {
         viewModel.list.observe(this) {
             Log.d(TAG, "list: list size: ${it.size}")
         }
+
+        // TODO: FATAL: test-only. remove it after finished.
+        binding.testItem1.setAsProgress()
+        binding.testItem2.setAsIncubating()
+    }
+
+    private fun ItemCourseListBinding.setAsProgress() {
+        val color = ContextCompat.getColor(this@MainActivity, R.color.teal)
+        textStatus.text = getString(R.string.desc_course_status_success)
+        textStatus.backgroundTintList = ColorStateList.valueOf(color)
+        textTitle.text = "進行中課程"
+        progressBar.setIndicatorColor(color)
+        progressBar.progress = 65
+        progressBar.max = 100
+        textProgress.text = getString(R.string.desc_course_progress, "65")
+        textCountdown.isInvisible = true
+    }
+
+    private fun ItemCourseListBinding.setAsIncubating() {
+        val color = ContextCompat.getColor(this@MainActivity, R.color.orange)
+        textStatus.text = getString(R.string.desc_course_status_incubating)
+        textStatus.backgroundTintList = ColorStateList.valueOf(color)
+        textTitle.text = "募資中課程"
+        progressBar.setIndicatorColor(color)
+        progressBar.progress = 10
+        progressBar.max = 30
+        textProgress.text = getString(R.string.desc_course_incubating_progress_people, 10, 30)
+        textCountdown.isInvisible = false
+        textCountdown.text = getString(R.string.desc_course_countdown, "13天")
     }
 
     private fun loadData() {
