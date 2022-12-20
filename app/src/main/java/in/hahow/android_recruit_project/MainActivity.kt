d4d2449a3af91bdc1e9bcc63dc0583975d577cb9
Samuel Chou <samuelchou@hotmail.com.tw>
2022-12-20 11:34:05 +0800

    feat(app): 初步架設入口網站。

---
+++ app/src/main/java/in/hahow/android_recruit_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.hahow.android_recruit_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
