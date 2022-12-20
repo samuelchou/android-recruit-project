aaf329d808e116cb0c887d4da1e74f4fb09e46f7
Samuel Chou <samuelchou@hotmail.com.tw>
2022-12-20 12:48:05 +0800

    feat(list/model): 新增窗口方法，為未來串接做準備。

--- app/src/main/java/in/hahow/android_recruit_project/repository/CourseListLoaderRepository.kt
+++ app/src/main/java/in/hahow/android_recruit_project/repository/CourseListLoaderRepository.kt
@@ -1,5 +1,9 @@
 package `in`.hahow.android_recruit_project.repository
 
+import `in`.hahow.android_recruit_project.data.CourseListBundle
+
 interface CourseListLoaderRepository {
     fun debugMessage(): String
+
+    fun getDisplayListBundle(): CourseListBundle
 }
--- app/src/main/java/in/hahow/android_recruit_project/repository/CourseListLoaderRepositoryImpl.kt
+++ app/src/main/java/in/hahow/android_recruit_project/repository/CourseListLoaderRepositoryImpl.kt
@@ -1,5 +1,11 @@
 package `in`.hahow.android_recruit_project.repository
 
+import `in`.hahow.android_recruit_project.data.CourseListBundle
+
 class CourseListLoaderRepositoryImpl : CourseListLoaderRepository {
     override fun debugMessage(): String = "CourseListLoaderRepositoryImpl"
+
+    override fun getDisplayListBundle(): CourseListBundle {
+        TODO("Not yet implemented")
+    }
 }
